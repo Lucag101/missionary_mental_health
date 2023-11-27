be64d4c6b4e42451b80cd304a94f04b3cb6648f4
Lucas Gandola <lucasbg@byu.edu>
2023-11-26 18:37:45 -0700

    small change

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,10 +4,11 @@ import './App.css';
 function App() {
   return (
     <div>
+      <p>navigation bar</p>
       <h1>Missionary Mental Health</h1>
       <p>Basic front page</p>
+      <p></p>
     </div>
-    
   );
 }
 
