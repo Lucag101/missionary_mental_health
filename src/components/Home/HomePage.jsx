683b7d855c3953172d4fad30bba649b617f4f9c8
davemurdock55 <davemurdock55@gmail.com>
2023-12-04 09:53:12 -0700

    Fixed homepage

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,24 +1,21 @@
 import React, { useState } from "react";
 import MoodSelector from "./MoodSelector";
-import '../../styles/sharedStyles.css'
+import "../../styles/sharedStyles.css";
 
 function HomePage() {
-
-
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-grow flex items-center justify-center text-cyan-900">
+      <div className="flex items-center justify-center flex-grow text-cyan-900">
         <div className="text-center">
-          <h1 className="text-3xl font-bold mb-4">Welcome!</h1>
+          <h1 className="mb-4 text-3xl font-bold">Welcome!</h1>
           <h2 className="text-xl">Time to focus on your mental health</h2>
-          {displayMoodModal && <MoodSelector onSelect={handleMoodSelection} onSubmit={handleMoodSubmit} onClose={handleCloseModal} />}
         </div>
       </div>
-      <div className="bg-white p-4 w-full">
-        <div className="flex justify-around items-center">
-          <button className="text-white py-2 px-4 bg-blue-500 rounded-md">Tab 1</button>
-          <button className="text-white py-2 px-4 bg-blue-500 rounded-md">Tab 2</button>
-          <button className="text-white py-2 px-4 bg-blue-500 rounded-md">Tab 3</button>
+      <div className="w-full p-4 bg-white">
+        <div className="flex items-center justify-around">
+          <button className="px-4 py-2 text-white bg-blue-500 rounded-md">Tab 1</button>
+          <button className="px-4 py-2 text-white bg-blue-500 rounded-md">Tab 2</button>
+          <button className="px-4 py-2 text-white bg-blue-500 rounded-md">Tab 3</button>
         </div>
       </div>
     </div>
