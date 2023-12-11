03f650b1136e64f46514ae78c6ccdc544e9e5ca2
Lucag101 <Lucag202@gmail.com>
2023-12-11 12:28:06 -0700

    teeny changes

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import MoodSelector from "./MoodSelector";
 import "../../styles/sharedStyles.css";
 
 function HomePage({displayMoodModal}) {
+  const [timerDone, setTimerDone] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimerDone(true);
+    }, 1000);
+    return () => clearTimeout(timer);
+  })
+
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex items-center justify-center flex-grow">
         <div className="text-center">
           <h1 className="text-3xl font-bold mb-4 transition-all duration-500">Welcome!</h1>
-          {!displayMoodModal &&  (
+          {(!displayMoodModal) &&  (
             <div>
             <h2 className="animate-pop-up text-xl pb-3">The mission field can be tough</h2>
             <h2 className="animate-pop-up-2 text-xl pb-3">It's time to focus on your mental health</h2>
